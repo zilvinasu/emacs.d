35f458c3603a8de743d8c8275e139badd39636ee
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-01-31 00:22:16 +0200

    Add package-archives to init.el

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+	("marmalade" . "http://marmalade-repo.org/packages/")
+	("melpa" . "http://melpa.milkbox.net/packages/")))
+
 (add-to-list 'load-path "~/.emacs.d/core/")
 
 (require 'setup-core)				   
