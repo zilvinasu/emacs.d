ca5506f071b2fcf50fb6a20709cd83abb9d156d8
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-02-01 21:07:59 +0200

    Install and enable global-flycheck-mode

--- emacs-custom.el
+++ emacs-custom.el
@@ -7,9 +7,14 @@
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(fringe-mode (quote (1 . 1)) nil (fringe))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote stack-ghci))
+ '(haskell-tags-on-save t)
  '(package-selected-packages
    (quote
-    (company-ghc haskell-mode exec-path-from-shell helm-ag elpy hydra cider clojure-mode dockerfile-mode company-go go-mode company-tern company web-mode smartparens restclient avy magit helm-projectile projectile expand-region helm smart-mode-line))))
+    (flycheck company-ghc haskell-mode exec-path-from-shell helm-ag elpy hydra cider clojure-mode dockerfile-mode company-go go-mode company-tern company web-mode smartparens restclient avy magit helm-projectile projectile expand-region helm smart-mode-line))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/setup-misc.el
+++ lisp/setup-misc.el
@@ -1,5 +1,5 @@
-
 (add-hook 'after-init-hook 'global-company-mode)
 (add-hook 'after-init-hook 'yas-global-mode)
+(add-hook 'after-init-hook 'global-flycheck-mode)
 
 (provide 'setup-misc)
