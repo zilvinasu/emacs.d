48a6cc03ee45680f8beffbf12469a701545740a9
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-02-01 15:35:52 +0200

    Bind M-x to helm-M-x

--- lisp/setup-kbds.el
+++ lisp/setup-kbds.el
@@ -1,7 +1,8 @@
 (windmove-default-keybindings)
 (global-unset-key (kbd "C-x c"))
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-set-key (kbd "M-i") 'imenu)
 (global-set-key (kbd "C-c l p") 'package-list-packages)
+(global-set-key (kbd "M-i") 'imenu)
+(global-set-key (kbd "M-x") 'helm-M-x)
 
 (provide 'setup-kbds)
