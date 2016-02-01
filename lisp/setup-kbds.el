e5b44757c55a3456c9c80148c3c83da658344e94
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-02-01 11:48:02 +0200

    Set helm command prefix to C-c h

--- lisp/setup-kbds.el
+++ lisp/setup-kbds.el
@@ -1,4 +1,6 @@
 (windmove-default-keybindings)
+(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-set-key (kbd "M-i") 'imenu)
 (global-set-key (kbd "C-c l p") 'package-list-packages)
 
