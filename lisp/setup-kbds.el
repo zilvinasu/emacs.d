24b8feb598c4e2ae18aa4c7b3c712c3a3dcee1db
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-02-01 20:22:07 +0200

    Install exec-path-from-shell

--- lisp/setup-kbds.el
+++ lisp/setup-kbds.el
@@ -6,7 +6,7 @@
 (global-set-key (kbd "C-c l p") 'package-list-packages)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-'") 'avy-goto-char-time)
+(global-set-key (kbd "C-'") 'avy-goto-char-timer)
 (global-set-key (kbd "C-=") 'er/expand-region)
 (global-set-key (kbd "C--") 'er/contract-region)
 (global-set-key (kbd "M-i") 'imenu)
