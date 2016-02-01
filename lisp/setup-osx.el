251db1d1287e03fc69da6a475b020d8ec043737e
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-02-01 20:29:09 +0200

    Fix OSX PATH env variable problems

--- lisp/setup-osx.el
+++ lisp/setup-osx.el
@@ -1,6 +1,13 @@
 (set-frame-font "Input Mono Narrow-12")
+
+;; Rebind modifier keys
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
+
+;; Fixup PATH
+(setq exec-path-from-shell-check-startup-files nil)
+(exec-path-from-shell-initialize)
+(exec-path-from-shell-copy-envs '("WORKON_HOME" "GOPATH"))
 
 (provide 'setup-osx)
