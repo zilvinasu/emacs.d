7eb800650f1ea4c0b5418f1fd4b56b64d3ab02f6
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-02-01 11:34:47 +0200

    Add global binding for package-list-packages

--- lisp/setup-kbds.el
+++ lisp/setup-kbds.el
@@ -1,4 +1,5 @@
 (windmove-default-keybindings)
-(global-set-key (kbd "M-i") 'imenu) 
+(global-set-key (kbd "M-i") 'imenu)
+(global-set-key (kbd "C-c l p") 'package-list-packages)
 
 (provide 'setup-kbds)
