29599107b89a6c60954d8e78f124686723ec6788
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-02-04 20:33:59 +0200

    Install use-package

--- emacs-custom.el
+++ emacs-custom.el
@@ -14,7 +14,7 @@
  '(haskell-tags-on-save t)
  '(package-selected-packages
    (quote
-    (yaml-mode helm-swoop go-eldoc flycheck company-ghc haskell-mode exec-path-from-shell helm-ag elpy hydra cider clojure-mode dockerfile-mode company-go go-mode company-tern company web-mode smartparens restclient avy magit helm-projectile projectile expand-region helm smart-mode-line))))
+    (use-package yaml-mode helm-swoop go-eldoc flycheck company-ghc haskell-mode exec-path-from-shell helm-ag elpy hydra cider clojure-mode dockerfile-mode company-go go-mode company-tern company web-mode smartparens restclient avy magit helm-projectile projectile expand-region helm smart-mode-line))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
