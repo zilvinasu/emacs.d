6c9cfcbe83ee36b4a9ff70c1937832fd6520e203
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-02-04 20:50:14 +0200

    Refactor setuo-docker for use-package

--- lisp/setup-docker.el
+++ lisp/setup-docker.el
@@ -3,8 +3,9 @@
 ;;; Docker mode related configs
 ;;; Code:
 
-(require 'dockerfile-mode)
-(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+(use-package dockerfile-mode
+  :init
+  (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
 
 (provide 'setup-docker)
 ;;; setup-docker.el ends here
