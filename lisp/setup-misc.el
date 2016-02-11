7c0456064fe126c2713329a6840eef6c3d23c4cb
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-02-11 23:06:17 +0200

    Enable company-mode only for prog modes

--- lisp/setup-misc.el
+++ lisp/setup-misc.el
@@ -3,9 +3,9 @@
 ;;; Configs for modes which require little configuration
 ;;; Code:
 (add-hook 'prog-mode-hook 'flycheck-mode)
+(add-hook 'prog-mode-hook 'company-mode)
 (add-hook 'text-mode-hook 'flyspell-mode)
 (winner-mode 1)
-(global-company-mode)
 (yas-global-mode)
 
 (provide 'setup-misc)
