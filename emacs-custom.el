1554eb64bb59e0949ea0fc8932634a8e685c1c1f
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-04-30 23:16:06 +0300

    packages: install json-mode

--- emacs-custom.el
+++ emacs-custom.el
@@ -11,7 +11,7 @@
  '(haskell-tags-on-save t)
  '(package-selected-packages
    (quote
-    (gitignore-mode tide evil-leader clj-refactor markdown-mode rainbow-delimiters evil evil-matchit evil-smartparens evil-surround org use-package yaml-mode helm-swoop go-eldoc flycheck company-ghc haskell-mode exec-path-from-shell helm-ag elpy hydra cider clojure-mode dockerfile-mode company-go go-mode company-tern company web-mode smartparens restclient avy magit helm-projectile projectile expand-region helm smart-mode-line))))
+    (json-mode gitignore-mode tide evil-leader clj-refactor markdown-mode rainbow-delimiters evil evil-matchit evil-smartparens evil-surround org use-package yaml-mode helm-swoop go-eldoc flycheck company-ghc haskell-mode exec-path-from-shell helm-ag elpy hydra cider clojure-mode dockerfile-mode company-go go-mode company-tern company web-mode smartparens restclient avy magit helm-projectile projectile expand-region helm smart-mode-line))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
