9f27f17f7e78e86ebb24de01e2f948c6be1b9c6b
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-05-08 01:06:21 +0300

    setup-editor: update auto save and backup dirs, cleanup confs

--- lisp/setup-editor.el
+++ lisp/setup-editor.el
@@ -12,12 +12,10 @@
 (sml/setup)
 
 ;; Backups
-(setq backup-directory-alist `(("." . "~/.emacs.d/.saves")))
-(setq backup-by-copying t)
-(setq delete-old-versions t
-      kept-new-versions 6
-      kept-old-versions 2
-      version-control t)
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;; Tabs
 (setq-default indent-tabs-mode nil)
