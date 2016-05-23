83b0c5302ff5f74f260e62aeaded38723bd8d92f
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-05-23 21:21:12 +0300

    setup-js: cleanup cfg and enable js2 node externs

--- lisp/setup-js.el
+++ lisp/setup-js.el
@@ -2,16 +2,17 @@
 ;;; Commentary:
 ;;; JavaScript lang related modes and configurations
 ;;; Code:
+(setq-default js-indent-level 2)
+(setq-default js2-include-node-externs t)
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
 (add-hook 'js-mode-hook
           (lambda ()
             (tern-mode t)
             (add-to-list 'company-backends 'company-tern)
-            (setq-local js-indent-level 2)
-            (setq-local evil-shift-width typescript-indent-level)
-            (setq-local indent-line-function 'js-jsx-indent-line)))
-
-(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+            (setq-local evil-shift-width js-indent-level)))
 
 (add-hook 'typescript-mode-hook
           (lambda ()
