f9caba9d5d80d9f7a053767ee6bb0e1372db96bd
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-05-24 23:55:09 +0300

    setup-haskell: cleanup cfgs

--- emacs-custom.el
+++ emacs-custom.el
@@ -6,7 +6,7 @@
  '(fringe-mode (quote (1 . 1)) nil (fringe))
  '(package-selected-packages
    (quote
-    (rich-minority nlinum nlinum-relative js2-mode json-mode gitignore-mode tide evil-leader clj-refactor markdown-mode rainbow-delimiters evil evil-matchit evil-smartparens evil-surround org use-package yaml-mode helm-swoop go-eldoc flycheck company-ghc haskell-mode exec-path-from-shell helm-ag elpy hydra cider clojure-mode dockerfile-mode company-go go-mode company-tern company web-mode smartparens restclient avy magit helm-projectile projectile expand-region helm))))
+    (rich-minority js2-mode json-mode gitignore-mode tide evil-leader clj-refactor markdown-mode rainbow-delimiters evil evil-matchit evil-smartparens evil-surround org use-package yaml-mode helm-swoop go-eldoc flycheck company-ghc haskell-mode exec-path-from-shell helm-ag elpy hydra cider clojure-mode dockerfile-mode company-go go-mode company-tern company web-mode smartparens restclient avy magit helm-projectile projectile expand-region helm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/setup-haskell.el
+++ lisp/setup-haskell.el
@@ -2,37 +2,41 @@
 ;;; Commentary:
 ;;; Haskell lang related modes and configurations
 ;;; Code:
-(custom-set-variables
- '(haskell-tags-on-save t)
- '(haskell-process-type 'stack-ghci)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t))
+(use-package haskell
+  :init
+  (setq-default haskell-tags-on-save t)
+  (setq-default haskell-process-type 'stack-gchi)
+  (setq-default haskell-process-suggest-remove-import-lines t)
+  (setq-default haskell-process-auto-import-loaded-modules t)
+  (setq-default haskell-process-log t)
+  :config
+  (autoload 'ghc-init "ghc" nil t)
+  (autoload 'ghc-debug "ghc" nil t)
+  (add-hook 'haskell-mode-hook
+            (lambda ()
+              (ghc-init)
+              (add-to-list 'company-backends 'company-ghc)))
 
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook
-          (lambda ()
-            (ghc-init)
-            (add-to-list 'company-backends 'company-ghc)))
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+  (eval-after-load 'haskell-mode
+    '(progn
+       (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+       (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+       (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+       (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+       (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+       (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+       (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+  (eval-after-load 'haskell-cabal
+    '(progn
+       (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+       (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+       (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+       (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal))))
 
-(eval-after-load 'haskell-mode '(progn
-  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
-  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
-  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
-  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
-  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
-(eval-after-load 'haskell-cabal '(progn
-  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
 (provide 'setup-haskell)
 ;;; setup-haskell.el ends here
