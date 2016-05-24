ce3f14f27d64fddf53f36080e0db021456001a21
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2016-05-24 23:42:32 +0300

    emacs-custom: cleanup unused configs

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,11 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(fringe-mode (quote (1 . 1)) nil (fringe))
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-type (quote stack-ghci))
- '(haskell-tags-on-save t)
  '(package-selected-packages
    (quote
     (rich-minority nlinum nlinum-relative js2-mode json-mode gitignore-mode tide evil-leader clj-refactor markdown-mode rainbow-delimiters evil evil-matchit evil-smartparens evil-surround org use-package yaml-mode helm-swoop go-eldoc flycheck company-ghc haskell-mode exec-path-from-shell helm-ag elpy hydra cider clojure-mode dockerfile-mode company-go go-mode company-tern company web-mode smartparens restclient avy magit helm-projectile projectile expand-region helm))))
