411b70dcfdca7ffcd442e6a0df02528b8102ecf0
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-10-23 15:42:12 +0300

    feat(init): setup custom installed packages dir

--- init.el
+++ init.el
@@ -8,9 +8,9 @@
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(setq package-user-dir "~/.emacs.d-packages")
 
 (add-to-list 'load-path  "~/.emacs.d/lisp/")
-
 (package-initialize)
 
 (setq custom-file "~/.emacs.d/emacs-custom.el")
